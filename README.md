eaa33bba96c85af13022189b5b277919f10afed7
Jeremy R <jeremy@devmounta.in>
2016-06-03 11:08:49 -0600

    adding fixes file and master README

---
+++ README.md
@@ -0,0 +1,16 @@
+## Welcome to the debugging challenge!
+
+### Rules
+
+* No cheating, no helping your neighbor or getting help.
+* Google is 100% okay
+* Looking at other people's repositories for this challenge is not okay.
+* To get credit for a fix you must document it in the fixes.txt file
+    * You must include what the bug was, what steps you took to find the bug, how you solved it.
+* Final solution should be sumitted via pull request back to the master repo.
+
+### Scoring
+
+* To win, all bugs must be found, solved and documented.
+* In the case of no one finishing, the winner will be the developer with the most bugs fixed and documented before the hour is up. 
+* In the case of a tie, the winner will be determined by who has the best documentation in their fixes.txt files.
--- challenge2/controllers/SightingCtrl.js
+++ challenge2/controllers/SightingCtrl.js
@@ -1,43 +1,44 @@
 var Sighting = require('../models/Sighting');
 
+
 module.exports = {
-  create: function(req, res) {
+  create: function (req, res) {
     var newSighting = new Sighting(req.body);
-    newSighting.save( function(err, result) {
-      if (err) {
-         res.status(500).send(err);
+    newSighting.save(function (err, result) {
+      if (!err) {
+        res.send(result);
       }
-      res.send(result);
+      res.status(500).send(err);
     });
   },
 
-  read: function(req, res) {
+  read: function (req, res) {
     console.log('req.query: ', req.query);
     Sighting.find(req.query)
-    .populate('user', 'username')
-    .exec(function(err, result) {
-      if (err) { 
+      .populate('user', 'username')
+      .exec(function (err, result) {
+        if (!err) {
+          res.send(result);
+        }
         res.status(500).send(err);
+      });
+  },
+
+  update: function (req, res) {
+    Sighting.findByIdAndUpdate(req.params.id, req.body, function (err, result) {
+      if (!err) {
+        res.send(result);
       }
-      res.send(result);
+      res.status(500).send(err);
     });
   },
 
-  update: function(req, res) {
-    Sighting.findByIdAndUpdate(req.params.id, req.body, function(err, result) {
-      if (err) {
-        res.status(500).send(err);
+  delete: function (req, res) {
+    Sighting.findByIdAndRemove(req.params.id, function (err, result) {
+      if (!err) {
+        res.send(result);
       }
-      res.send(result);
-    });
-  },
-
-  delete: function(req, res) {
-    Sighting.findByIdAndRemove(req.params.id, function(err, result) {
-      if (err){ 
-        res.status(500).send(err);
-      }
-      res.send(result);
+      res.status(500).send(err);
     });
   }
 };
