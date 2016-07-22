8cfd805933327cb97988abf257d9b35bc1ca8fa8
Joseph Toh Djojo <joseph.tohdjojo@gmail.com>
2016-07-22 12:01:15 -0600

    fixed challenge1

--- challenge1/js/app.js
+++ challenge1/js/app.js
@@ -1,22 +1,22 @@
-angular.module("miniRouting", ['ui-router'])
+angular.module('miniRouting', ['ui.router']) // should be ui.router not ui-router
 
-.config(function($stateProvider){
+	.config( function($stateProvider) {
 
-    $stateProvider
-        .state('home') {
-            url: '/',
-            templateUrl: 'js/home/homeTmpl.html',
-            controller: 'homeCtrl'
-        })
-        .state('settings') {
-            url: '/settings',
-            templateUrl: 'js/settings/settingsTmpl.html',
-            controller: 'settingsctrl'
-        })            
-        })
-         .state('products') {
-            url: '/products/:id',
-            templateUrl: 'js/products/productTmpl.html',
-            controller: 'productsCtrl'
-        })
-    
+	    $stateProvider
+	        .state('home', {
+	            url: '/',
+	            templateUrl: 'js/home/homeTmpl.html',
+	            controller: 'homeCtrl'
+	        })
+	        .state('settings', {
+	            url: '/settings',
+	            templateUrl: 'js/settings/settingsTmpl.html',
+	            controller: 'settingsctrl'
+	        })
+	        .state('products', {
+	            url: '/products/:id',
+	            templateUrl: 'js/products/productTmpl.html',
+	            controller: 'productsCtrl'
+	        });
+	});
+// A whole lot of parenthesis in the wrong place. missing closing brackets/parenthesis
