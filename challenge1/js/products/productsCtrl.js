f1e5d20abc99dbc7580dfbe8a79050540ae4b6bb
Cameron Gale <cameroneldongale@gmail.com>
2016-07-22 12:36:06 -0600

    debuggin changes

--- challenge1/js/products/productsCtrl.js
+++ challenge1/js/products/productsCtrl.js
@@ -1,9 +1,12 @@
-angular.module('miniRouting').controller("productsCtrl", function($scope, $stateParams){
+angular.module('miniRouting').controller("productsCtrl", function($scope, $stateParams, productService){
     var productType = $stateParams.id;
-    
+
     if(productType === "shoes"){
         $scope.productData = productService.shoeData;
     } else {
         $scope.productData = productService.sockData;
     }
-);
+});
+
+
+//added a squiggly on line 9
