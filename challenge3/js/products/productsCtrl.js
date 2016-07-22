eed63e615622ec364d89e68f0b32cb2fb271ccec
tsingleton5 <tjsingleton5@gmail.com>
2016-07-22 12:45:40 -0600

    debugging challenges

--- challenge3/js/products/productsCtrl.js
+++ challenge3/js/products/productsCtrl.js
@@ -1,9 +1,17 @@
 angular.module('miniRouting').controller("productsCtrl", function($scope, $stateParams, productService){
     var productType = $stateParams.id;
-    
+
     if(productType === "shoes"){
-        $scope.productData = productService.sheoData;
-    } else {
+        $scope.productData = productService.shoeData;
+    } else if (productType === "socks") {
         $scope.productData = productService.sockData;
     }
+
+
+    // if ($stateParams.id === 'shoes') {
+    //     $scope.productData = productService.shoeData;
+    // }
+    // else if ($stateParams.id === 'socks') {
+    //     $scope.productData = productService.sockData;
+    // }
 });
