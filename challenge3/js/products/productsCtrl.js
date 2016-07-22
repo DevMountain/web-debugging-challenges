38c2768ff1feb30a6ef0db75fdd2c67a253340e3
Joseph Toh Djojo <joseph.tohdjojo@gmail.com>
2016-07-22 12:24:39 -0600

    finished challenge3

--- challenge3/js/products/productsCtrl.js
+++ challenge3/js/products/productsCtrl.js
@@ -1,8 +1,8 @@
 angular.module('miniRouting').controller("productsCtrl", function($scope, $stateParams, productService){
     var productType = $stateParams.id;
-    
+
     if(productType === "shoes"){
-        $scope.productData = productService.sheoData;
+        $scope.productData = productService.shoeData; // shoeData not sheoData. this one i just caught on my own. no real debugging here.
     } else {
         $scope.productData = productService.sockData;
     }
