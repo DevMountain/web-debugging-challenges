4452e762ea078c7223516bbfc3c18d9892351e51
mrevanwitt <mrevanwitt@gmail.com>
2016-09-12 12:35:34 -0600

    finished 1, 3, and 4.

--- challenge1/js/products/productsCtrl.js
+++ challenge1/js/products/productsCtrl.js
@@ -1,9 +1,12 @@
-angular.module('miniRouting').controller("productsCtrl", function($scope, $stateParams){
+angular.module('miniRouting')
+  .controller("productsCtrl", function($scope, $stateParams, productService) {
+    // console.log($stateParams.id)
     var productType = $stateParams.id;
-    
+    console.log(typeof productType)
+
     if(productType === "shoes"){
         $scope.productData = productService.shoeData;
     } else {
         $scope.productData = productService.sockData;
     }
-);
+});
--- challenge3/js/products/productsCtrl.js
+++ challenge3/js/products/productsCtrl.js
@@ -1,8 +1,9 @@
 angular.module('miniRouting').controller("productsCtrl", function($scope, $stateParams, productService){
     var productType = $stateParams.id;
-    
+    console.log(productType)
+
     if(productType === "shoes"){
-        $scope.productData = productService.sheoData;
+        $scope.productData = productService.shoeData;
     } else {
         $scope.productData = productService.sockData;
     }
