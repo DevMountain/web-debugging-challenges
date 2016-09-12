3236f7655df768b545583fea8ab877afd67b4082
Candice Humpherys <smrtsmrf@gmail.com>
2016-09-12 12:33:48 -0600

    fixed the app.js

--- challenge1/js/app.js
+++ challenge1/js/app.js
@@ -1,21 +1,44 @@
-angular.module("miniRouting", ['ui-router'])
+// angular.module("miniRouting", ['ui-router'])
+
+// .config(function($stateProvider){
+
+//     $stateProvider
+//         .state('home', {
+//             url: '/',
+//             templateUrl: 'js/home/homeTmpl.html',
+//             controller: 'homeCtrl'
+//         })
+//         .state('settings', {
+//             url: '/settings',
+//             templateUrl: 'js/settings/settingsTmpl.html',
+//             controller: 'settingsctrl'
+//         })            
+//          .state('products', {
+//             url: '/products/:id',
+//             templateUrl: 'js/products/productTmpl.html',
+//             controller: 'productsCtrl'
+//         })
+//     })
+angular.module("miniRouting", ['ui.router'])
 
 .config(function($stateProvider){
 
     $stateProvider
         .state('home', {
             url: '/',
-            templateUrl: 'js/home/homeTmpl.html',
+            templateUrl: 'js/home/homeTemplate.html',
             controller: 'homeCtrl'
         })
         .state('settings', {
             url: '/settings',
             templateUrl: 'js/settings/settingsTmpl.html',
-            controller: 'settingsctrl'
-        })            
+            controller: 'settingsCtrl'
+        })
          .state('products', {
-            url: '/products/:id',
+            url: '/products/:productid',
             templateUrl: 'js/products/productTmpl.html',
             controller: 'productsCtrl'
         })
-    })
+    
+    
+})
